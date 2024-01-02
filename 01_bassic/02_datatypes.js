f3aa097412bede3c806d3c0132de11ea3c6c8c71
Pavansingh8938 <155299696+Pavansingh8938@users.noreply.github.com>
2024-01-02 14:41:28 +0000

    datat types

--- 01_bassic/02_datatypes.js
+++ 01_bassic/02_datatypes.js
@@ -1,3 +1,23 @@
 "user strict";//treat all the js code in the new version
 
 //alert() we are using in node js not browser
+
+let name="pavan"
+let agr=18
+let isloggedin=false
+
+//num 2 to power 53
+//bigint use for biggest num exaple for(facebook,insta)
+//string =""
+//boolean= true/false
+//null = standloane of /khali (empty)
+//undefiedn = variable declare but ni given put the value 
+//symbol = uniqe ke liye
+
+
+
+
+//object
+
+console.log(typeof undefined);//undefined is a undefined
+console.log(typeof null);//it is a object
