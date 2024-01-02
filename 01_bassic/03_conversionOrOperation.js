37814a05be1fb412d15b5da0c5ead0f34de9aadf
Pavansingh8938 <155299696+Pavansingh8938@users.noreply.github.com>
2024-01-02 15:37:48 +0000

    operations

--- 01_bassic/03_conversionOrOperation.js
+++ 01_bassic/03_conversionOrOperation.js
@@ -1,18 +1,47 @@
-let age = true
- console.log(typeof age);//string
- console.log(typeof (age));//string
+// let age = true
+//  console.log(typeof age);//string
+//  console.log(typeof (age));//string
 
- let number=Number(age);
- console.log(typeof number);//number
- console.log(number);//NaN (not a number)
- console.log(number);//0 show put value is null
- console.log(number);//NaN show put value is undefined
- console.log(number);//1 show when the put value  is true
- console.log(number);//0 show when the put value is false
-//1 = true;
-// 0 = flase;
-// "" = flase;
-// "pavan"=true;
+//  let number=Number(age);
+//  console.log(typeof number);//number
+//  console.log(number);//NaN (not a number)
+//  console.log(number);//object show put value is null
+//  console.log(number);//NaN show put value is undefined
+//  console.log(number);//1 show when the put value  is true
+//  console.log(number);//0 show when the put value is false
+// //1 = true;
+// // 0 = flase;
+// // "" = flase;
+// // "pavan"=true;
 
+//******************************operation****************************//
 
+let value= 3;
+let negative=-value
+console.log(negative);
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+// console.log(2*2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+// let gamecounter=100
+// gamecounter++;
+// console.log(gamecounter);
+
+// let gamecounter=100
+// ++gamecounter;
+// console.log(gamecounter);
+
+//link to study
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
