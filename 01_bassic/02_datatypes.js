8dceca7f527d5cd678f496e32369a87235b1bac4
Pavansingh8938 <155299696+Pavansingh8938@users.noreply.github.com>
2024-01-02 14:30:32 +0000

    container config

---
+++ 01_bassic/02_datatypes.js
@@ -0,0 +1,3 @@
+"user strict";//treat all the js code in the new version
+
+//alert() we are using in node js not browser
--- 01_bassic/text.js
+++ 01_bassic/text.js
@@ -1,1 +1,16 @@
-console.log('hellow')
+const account=144553
+let accountemail="pavan@gmail.com"
+var accountpassword="123456"
+acountcity="Jaipur"
+
+accountemail="pa@gmail.com"
+accountpassword="126"
+acountcity="gj"
+// account=2
+let accountState;
+/*
+prefer not to use var
+because of issue in block space and funtional space
+*/
+console.log(account);
+console.table([accountemail,accountpassword,acountcity])
