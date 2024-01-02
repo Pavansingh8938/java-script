918292908d70af45b26ab4afe186c7ef80eb12af
Pavansingh8938 <155299696+Pavansingh8938@users.noreply.github.com>
2024-01-02 15:02:46 +0000

    converstion of data types

---
+++ 01_bassic/03_conversionOrOperation.js
@@ -0,0 +1,18 @@
+let age = true
+ console.log(typeof age);//string
+ console.log(typeof (age));//string
+
+ let number=Number(age);
+ console.log(typeof number);//number
+ console.log(number);//NaN (not a number)
+ console.log(number);//0 show put value is null
+ console.log(number);//NaN show put value is undefined
+ console.log(number);//1 show when the put value  is true
+ console.log(number);//0 show when the put value is false
+//1 = true;
+// 0 = flase;
+// "" = flase;
+// "pavan"=true;
+
+
+
